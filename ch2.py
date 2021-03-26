25630c3e69bba39aed5563cc0350969d887962e4
Evgeniy Kudryavtsev <sky-ork@yandex.ru>
2021-03-27 01:08:18 +0800

    Edit ch2

--- ch2.py
+++ ch2.py
@@ -5,5 +5,6 @@ bash_command = ["cd ~/PycharmProjects/Test_DevOps/Test_DevOps", "git status"]
 result_os = os.popen(' && '.join(bash_command)).read()
 for result in result_os.split('\n'):
     if result.find('изменено') != -1:
-        prepare_result = result.replace('\tизменено:      ', '')
-        print('   ' + bash_command[0].replace('cd ', '') + '/' + prepare_result)
+        prepare_result = '   ' + bash_command[0].replace('cd ', '')\
+                         + '/' + result.replace('\tизменено:      ', '')
+        print(prepare_result)
