7cb1bc1078ab478d6344ec8b77eecc2ea31d6665
Evgeniy Kudryavtsev <sky-ork@yandex.ru>
2021-03-27 22:28:12 +0800

    ch4 OK!

--- HW_04.2/ch4.py
+++ HW_04.2/ch4.py
@@ -1,3 +1,42 @@
 #!/usr/bin/env python3
-import os
+import sys
+import socket
+import subprocess
+import time
 
+
+# Adding the URL and IP from the arguments to the dict
+def get_url_ip(url):
+    url_ip = {}
+    for count in range(len(url)):
+        exit_code = subprocess.run(['curl', '-Is', str(url[count])],
+                                   stdout=subprocess.DEVNULL)
+        if exit_code.returncode == 0:
+            url_ip[str(url[count])] = socket.gethostbyname(str(url[count]))
+    return dict(url_ip)
+
+
+cnt_ask = 0
+print('-' * 40)
+while cnt_ask < 100:
+    if cnt_ask == 0:
+        url_ip_dict = get_url_ip(sys.argv)
+        for item_key in url_ip_dict.keys():
+            result = item_key + " - " + url_ip_dict[item_key]
+            print(result)
+        url_ip_dict_coll = url_ip_dict
+    else:
+        url_ip_dict = get_url_ip(sys.argv)
+        for item_key in url_ip_dict.keys():
+            if url_ip_dict[item_key] == url_ip_dict_coll[item_key]:
+                result = item_key + " - " + url_ip_dict[item_key]
+                print(result)
+            else:
+                result = "[ERROR] " + item_key + " IP mismatch: " \
+                         + url_ip_dict_coll[item_key] + " --> " \
+                         + url_ip_dict[item_key]
+                print(result)
+        url_ip_dict_coll = url_ip_dict
+    print('-' * 40)
+    cnt_ask += 1
+    time.sleep(1)
